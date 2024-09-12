f77d45323b07d0a898170f320a331a6ca68b8928
Akay74 <akaudeh@gmail.com>
2024-09-12 12:20:01 +0100

    🔧 add a backgroundImage and font config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
+        "hero": "url('/assets/hero.jpg')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        sans: ['var(--font-lato)'],
       },
     },
   },
