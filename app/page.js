6c165ac3bbd4330debd692a4da11a84cb8a5ce67
Akay74 <akaudeh@gmail.com>
2024-09-13 15:12:01 +0100

    🎨 import and use Services component

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import Services from "@/components/Services";
 import Contact from "@/components/Contact";
 import Feedback from "@/components/Feedback";
 import Footer from "@/components/Footer";
@@ -10,6 +11,7 @@ export default function Home() {
     <main>
       <Hero />
       <About />
+      <Services />
       <Contact />
       <Feedback />
       <Footer />
