f6ea8d2ab7afa0ec7e6a0d62c4c173b2a8bfd8cb
Akay74 <akaudeh@gmail.com>
2024-09-13 16:44:53 +0100

    🎨 refactor form structure to fit larger screens

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -10,44 +10,48 @@ const Contact = () => {
         Cultivating Connections: Reach Out and Connect With Us.
       </h2>
 
-      <form className="max-w-lg mx-auto">
-        <div className="space-y-4">
-          <input
-            type="text"
-            placeholder="Name"
-            className="w-full p-3 border-none outline-none rounded bg-[#F9F9F9] bg-opacity-10"
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            className="w-full p-3 border-none outline-none rounded bg-[#F9F9F9] bg-opacity-10"
-          />
-          <input
-            type="tel"
-            placeholder="Phone number"
-            className="w-full p-3 border-none outline-none rounded bg-[#F9F9F9] bg-opacity-10"
-          />
-          <select
-            className="w-full p-3 text-[#C1BFBF] border-none outline-none rounded bg-[#F9F9F9] bg-opacity-10"
-            defaultValue=""
-          >
-            <option value="" disabled>Service of Interest</option>
-            {services.map((service, index) => (
-              <option key={index} value={service}>
-                {service}
-              </option>
-            ))}
-          </select>
-          <input
-            type="text"
-            placeholder="Timeline"
-            className="w-full p-3 border-none outline-none rounded bg-[#F9F9F9] bg-opacity-10"
-          />
-          <textarea
-            placeholder="Project Details"
-            rows="5"
-            className="w-full p-3 border-none outline-none rounded bg-[#F9F9F9] bg-opacity-10"
-          ></textarea>
+      <form className="mx-auto md:w-[80%]">
+        <div className="space-y-4 md:space-y-0 md:flex justify-center items-top">
+          <div className="space-y-4">
+            <input
+              type="text"
+              placeholder="Name"
+              className="w-full p-3 border-none outline-none rounded bg-[#F9F9F9] bg-opacity-10"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full p-3 border-none outline-none rounded bg-[#F9F9F9] bg-opacity-10"
+            />
+            <input
+              type="tel"
+              placeholder="Phone number"
+              className="w-full p-3 border-none outline-none rounded bg-[#F9F9F9] bg-opacity-10"
+            />
+          </div>
+          <div className="space-y-4">
+            <select
+              className="w-full p-3 text-[#C1BFBF] border-none outline-none rounded bg-[#F9F9F9] bg-opacity-10"
+              defaultValue=""
+            >
+              <option value="" disabled>Service of Interest</option>
+              {services.map((service, index) => (
+                <option key={index} value={service}>
+                  {service}
+                </option>
+              ))}
+            </select>
+            <input
+              type="text"
+              placeholder="Timeline"
+              className="w-full p-3 border-none outline-none rounded bg-[#F9F9F9] bg-opacity-10"
+            />
+            <textarea
+              placeholder="Project Details"
+              rows="5"
+              className="w-full p-3 border-none outline-none rounded bg-[#F9F9F9] bg-opacity-10"
+            ></textarea>
+          </div>
         </div>
 
         <div className="mt-6 text-center">
