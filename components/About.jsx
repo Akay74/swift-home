5f97d9ba4a72aaf66e067e173bc1179dcf36b943
Akay74 <akaudeh@gmail.com>
2024-09-15 23:51:02 +0100

    💄 update font sizes for about section

--- components/About.jsx
+++ components/About.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="container mx-auto mt-5 px-4 py-3 text-[#F9F9F9]" id='about-us'>
-      <h1 className="text-4xl text-center font-bold mb-4">About us</h1>
+    <div className="mx-auto mt-5 md:mx-[3rem] px-4 py-3 text-[#F9F9F9]" id='about-us'>
+      <h1 className="text-4xl text-center font-bold mb-4 md:text-[64px]">About us</h1>
       
-      <div className="mb-8">
+      <div className="mb-8 md:text-[20px]">
         <p className="mb-4">We are a leading tech solutions provider, shaping the future across the most in-demand niches in the tech space. Our mission is to equip businesses with powerful strategies that align with the latest market trends, deploying precise and innovative tactics to ensure visibility, authority, leadership, and success.</p>
         <p className="mb-4">We are driven by a commitment to excellence and an unwavering belief in charting unconventional paths to deliver real, unmatched value to our clients. This approach has earned us the trust and loyalty of our customers, leading to repeat business and long-lasting partnerships.</p>
         <p>In today's ever-changing global market, thriving requires intense collaboration, creative thinking, and cutting-edge digital upgrades. We are ready to be your partner in this journey, offering solutions that not only meet your needs but also set you apart from the competition.</p>
       </div>
 
-      <h2 className="text-2xl font-semibold mb-4">What we do</h2>
-      <p className="mb-8">We specialize in providing end-to-end tech services encompassing design, development, and digital marketing. From crafting captivating brand identities to developing robust web and mobile applications, we offer a comprehensive suite of services to propel your business forward in the digital age.</p>
+      <h2 className="text-2xl font-semibold mb-4 md:text-[48px]">What we do</h2>
+      <p className="mb-8 md:text-[20px]">We specialize in providing end-to-end tech services encompassing design, development, and digital marketing. From crafting captivating brand identities to developing robust web and mobile applications, we offer a comprehensive suite of services to propel your business forward in the digital age.</p>
 
-      <h2 className="text-2xl font-semibold mb-4">Why choose us?</h2>
-      <div>
+      <h2 className="text-2xl font-semibold mb-4 md:text-[48px]">Why choose us?</h2>
+      <div className="md:text-[20px]">
         <p className="mb-2">
           <span className="font-semibold">Innovation:</span> In a rapidly evolving global business environment, we set the pace by driving thought leadership and staying ahead of industry trends. Our commitment to employing cutting-edge strategies ensures that we deliver unmatched value, fostering a community of thriving businesses.
         </p>
