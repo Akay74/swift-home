102017089c0c5703e12793c0b22ab04a7e0dc348
Akay74 <akaudeh@gmail.com>
2024-09-16 19:53:29 +0100

    🎨 update bg url, remove unused style

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "hero": "url('/assets/hero.jpg')",
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "hero": "url('../assets/hero.jpg')",
       },
       fontFamily: {
         sans: ['var(--font-lato)'],
